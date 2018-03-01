875ee0c3d2dd74c0dfe1da375dcab1af0ba36869
iceyang <w@icey.cc>
2018-03-01 20:13:06 +0800

    意见反馈完成

--- src/pages/BusinessCenter/Options/options.vue
+++ src/pages/BusinessCenter/Options/options.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <h1>意见管理</h1>
+    <h3 class="small-title">意见反馈</h3>
+    <div class="text-box">
+      <el-input
+        class="text-tarea"
+        type="textarea"
+        :rows="10"
+        placeholder="如果您在使用中有任何问题，可以写下来反馈给我们，我们将持续改进"
+        v-model="textarea">
+      </el-input>
+      <el-input v-model="input" placeholder="您可以留下电话，方便我们联系您"></el-input>
+    </div>
+
+    <div class="op-btn">
+      <el-button type="primary">提交</el-button>
+    </div>
   </div>
 </template>
 <script>
@@ -8,8 +22,28 @@
     name:'',
     data(){
       return{
-        
+        textarea: '',
+        input:''
+
       }
     }
   }
 </script>
+<style lang="scss">
+
+  .text-box{
+    padding: 20px;
+    background: #fff;
+    .text-tarea{
+      margin-bottom: 10px;
+    }
+  }
+  .op-btn{
+    padding-top: 20px;
+    text-align: center;
+    .el-button{
+      width: 100px;
+    }
+  }
+
+</style>
